d1cd11535972687a9dd1cc4252ca8454bfc74955
CaioSEC <caiosecond13@gmail.com>
2026-05-23 00:28:14 -0300

    scriptures fixed

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -3,7 +3,153 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        {
+            Reference reference = new Reference("Proverbs", 3, 5, 6);
+            string text = "Trust in the Lord with all your heart and lean not on your own understanding in all your ways submit to him and he will make your paths straight";
+            
+            Scripture scripture = new Scripture(reference, text);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine();
+
+                if (scripture.IsCompletelyHidden())
+                {
+                    break;
+                }
+
+                Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+                string input = Console.ReadLine();
+
+                if (input.Trim().ToLower() == "quit")
+                {
+                    break;
+                }
+
+                scripture.HideRandomWords(3);
+            }
+        }
     }
+
+    public class Word
+    {
+        private string _text;
+        private bool _isHidden;
+
+        public Word(string text)
+        {
+            _text = text;
+            _isHidden = false;
+        }
+
+        public void Hide()
+        {
+            _isHidden = true;
+        }
+
+        public bool IsHidden()
+        {
+            return _isHidden;
+        }
+
+        public string GetDisplayText()
+        {
+            if (_isHidden)
+            {
+                return new string('_', _text.Length);
+            }
+            return _text;
+        }
+    }
+
+    public class Reference
+    {
+        private string _book;
+        private int _chapter;
+        private int _verse;
+        private int? _endVerse; 
+        public Reference(string book, int chapter, int verse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _verse = verse;
+            _endVerse = null;
+        }
+
+        public Reference(string book, int chapter, int startVerse, int endVerse)
+        {
+            _book = book;
+            _chapter = chapter;
+            _verse = startVerse;
+            _endVerse = endVerse;
+        }
+
+        public string GetDisplayText()
+        {
+            if (_endVerse == null)
+            {
+                return $"{_book} {_chapter}:{_verse}";
+            }
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+
+    public class Scripture
+    {
+        private Reference _reference;
+        private List<Word> _words;
+
+        public Scripture(Reference reference, string text)
+        {
+            _reference = reference;
+            _words = new List<Word>();
+
+            string[] textWords = text.Split(' ');
+            foreach (string word in textWords)
+            {
+                _words.Add(new Word(word));
+            }
+        }
+
+        public void HideRandomWords(int numberToHide)
+        {
+            Random random = new Random();
+            int hiddenCount = 0;
+
+            while (hiddenCount < numberToHide && !IsCompletelyHidden())
+            {
+                int randomIndex = random.Next(_words.Count);
+                
+                if (!_words[randomIndex].IsHidden())
+                {
+                    _words[randomIndex].Hide();
+                    hiddenCount++;
+                }
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            List<string> displayedWords = new List<string>();
+            foreach (Word word in _words)
+            {
+                displayedWords.Add(word.GetDisplayText());
+            }
+
+            return $"{_reference.GetDisplayText()} {string.Join(" ", displayedWords)}";
+        }
+
+        public bool IsCompletelyHidden()
+        {
+            foreach (Word word in _words)
+            {
+                if (!word.IsHidden())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
